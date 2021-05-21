7ab29be8523b3e19d2f582994fe46964b3f84c4e
momchilantonov <eng.antonov@gmail.com>
2021-05-21 23:32:41 +0300

    first lecture  work

--- django_training/first_app/admin.py
+++ django_training/first_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from django_training.first_app.models import Person
+
+admin.site.register(Person)
--- django_training/first_app/views.py
+++ django_training/first_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from django_training.first_app.models import Person
+
+
 def index(req):
     context = {
         "list": "List of persons:",
+        "people": Person.objects.all()
     }
     return render(req, "index.html", context)
