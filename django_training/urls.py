2f56d0833254d15aeab3b059ca3bfdada1a85ef6
momchilantonov <eng.antonov@gmail.com>
2021-05-25 09:16:14 +0300

    django urls conventions

--- django_training/urls.py
+++ django_training/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from django_training.first_app.views import index, list_phones
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("django_training.first_app.urls"))
+    path("first_app/", include("django_training.first_app.urls")),
+    path("people/", include("django_training.people.urls")),
 ]
