eeb7991bd1e9873ddb9336a0ae0e6cac0979c67a
momchilantonov <eng.antonov@gmail.com>
2021-05-26 23:56:08 +0300

    http get and post requests and upload resorces

--- django_training/first_app/models.py
+++ django_training/first_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import forms
 
 
 class Person(models.Model):
--- django_training/first_app/urls.py
+++ django_training/first_app/urls.py
@@ -5,7 +5,7 @@ from django_training.first_app.views import index, list_phones, create_person
 
 urlpatterns = [
     path("", index),
-    path("create/", create_person),
+    path("create/", create_person, name="create person"),
     path("phones/", list_phones),
     # Show template without write a func in view.py (show html page without python logic)
     # path("phones2/", TemplateView.as_view(template_name="phones.html")),
