b800130e068abec517fa7f328d08e13f85dba770
momchilantonov <eng.antonov@gmail.com>
2021-06-24 16:20:28 +0300

    make admin register and put some TODOS for the exam

--- web_basics_exam/web_basics_exam/main_app/admin.py
+++ web_basics_exam/web_basics_exam/main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+# from web_basics_exam.web_basics_exam.main_app.models import 'model'
 
-# Register your models here.
+
+@admin.register('model')
+class Admin(admin.ModelAdmin):
+    list_display = []
+    list_filter = []
+    sortable_by = []
